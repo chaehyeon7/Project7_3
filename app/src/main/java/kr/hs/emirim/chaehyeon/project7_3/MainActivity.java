9650e83061158240d60c4f2b0936d9b201b5057e
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-07-26 10:34:23 +0900

    Edit MainActivity.java(Dialog_positive_button Listener) Commit

--- app/src/main/java/kr/hs/emirim/chaehyeon/project7_3/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/project7_3/MainActivity.java
@@ -3,15 +3,17 @@ package kr.hs.emirim.chaehyeon.project7_3;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView textName, textEmail;
     View dialogV;
-
+    EditText editName, editEmal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +34,15 @@ public class MainActivity extends AppCompatActivity {
             dialog.setTitle(R.string.user_input);
             dialog.setIcon(R.drawable.user);
             dialog.setView(dialogV);
-            dialog.setPositiveButton("확인", null);
+            dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    editName = dialogV.findViewById(R.id.dialog_edit_name);
+                    editEmal = dialogV.findViewById(R.id.dialog_edit_email);
+                    textName.setText(editName.getText().toString());
+                    textEmail.setText(editEmal.getText().toString());
+                }
+            });
             dialog.setNegativeButton("취소", null);
             dialog.show();
         }
